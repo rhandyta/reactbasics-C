9f728294e1dfbf0184c89122390efe2e90d90bbf
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-12-12 07:19:01 +0700

    event handling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import "./App.css";
 import Card from './Components/Card';
 
 function App() {
+
+  const hello = () => {
+    console.log('Hello World')
+  }
+
     return (
         <>
-            <Card/>
+            <Button hello={hello}/>
         </>
     );
+}
+
+function Button (props) {
+  return (
+    <>
+      <button onClick={props.hello}>Click</button>
+    </>
+  )
 }
 
 export default App;
