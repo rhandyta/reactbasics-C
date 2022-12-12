057fa5cde0f72079c90ca3b75e1e2a080279d6cc
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-12-12 08:31:11 +0700

    Form Processing

--- src/Components/ListFood.js
+++ src/Components/ListFood.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import FoodItem from "./FoodItem";
 
 export default function ListFood() {
+    const [initialFood, setInitialFood] = useState('')
     const [foods, setFoods] = useState([
         {
             id: Math.random() * 100 + 1,
@@ -24,8 +25,24 @@ export default function ListFood() {
             harga: "10003"
         },
     ])
+
+    function foodHandler (e) {
+        setInitialFood(e.target.value)
+    }
+
+    function submitHandler (e) {
+        e.preventDefault();
+        setFoods([...foods, {id: Math.random() * 100 + 1, namaMakanan: initialFood, harga: "15000"}])
+        setInitialFood("")
+    }
+
+
     return (
         <>
+        <form onSubmit={submitHandler}>
+            <label htmlFor="food">Nama Makanan: </label>
+            <input id="food" type="text" onChange={foodHandler} value={initialFood} />
+        </form>
             {
                 foods.map((food, index) => {
                     return (
