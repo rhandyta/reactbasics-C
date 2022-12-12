4cf4c8b5af4c31654c5fc6e2aa457f1da1c33c5b
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-12-12 08:16:23 +0700

    use state array

--- src/Components/ListFood.js
+++ src/Components/ListFood.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import FoodItem from "./FoodItem";
 
 export default function ListFood() {
+    const [foods, setFoods] = useState([
+        {
+            id: Math.random() * 100 + 1,
+            namaMakanan: "Tempe",
+            harga: "10000"
+        },
+        {
+            id: Math.random() * 100 + 1,
+            namaMakanan: "Tahu",
+            harga: "10001"
+        },
+        {
+            id: Math.random() * 100 + 1,
+            namaMakanan: "Susu Kedelai",
+            harga: "10002"
+        },
+        {
+            id: Math.random() * 100 + 1,
+            namaMakanan: "Sambal Kacang",
+            harga: "10003"
+        },
+    ])
     return (
         <>
-            <div>
-                <FoodItem harga="10000">Tempe </FoodItem>
-                <FoodItem harga="9000">Tahu </FoodItem>
-                <FoodItem harga="15000">Burcang </FoodItem>
-                <FoodItem harga="5000">Samceng </FoodItem>
-            </div>
+            {
+                foods.map((food, index) => {
+                    return (
+                        <FoodItem key={index} harga={food.harga}>{food.namaMakanan}</FoodItem>
+                    )
+                })
+            }
         </>
     );
 }
